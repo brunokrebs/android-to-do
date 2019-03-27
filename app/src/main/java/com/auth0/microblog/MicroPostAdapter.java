8d427d1b4859a66fa42787da6478a18d51fa8e90
Bruno Krebs <bruno.krebs@auth0.com>
2019-03-26 23:53:58 -0300

    using glide to show profile image;

--- app/src/main/java/com/auth0/microblog/MicroPostAdapter.java
+++ app/src/main/java/com/auth0/microblog/MicroPostAdapter.java
@@ -5,16 +5,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MicroPostAdapter extends BaseAdapter {
+    private Context context;
     private LayoutInflater inflater;
     private List<MicroPost> microPosts = new ArrayList<>();
 
     MicroPostAdapter(Context context) {
+        this.context = context;
         inflater = LayoutInflater.from(context);
     }
 
@@ -27,6 +32,10 @@ public class MicroPostAdapter extends BaseAdapter {
 
         TextView message = view.findViewById(R.id.micro_post_message);
         message.setText(microPost.getMessage());
+
+        ImageView profilePicture = view.findViewById(R.id.profile_picture);
+        Glide.with(context).load("https://s.gravatar.com/avatar/d1ad971613b01e221226aa2f4700267a?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fsk.png").into(profilePicture);
+
         return view;
     }
 
